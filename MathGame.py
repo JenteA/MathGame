cce2c87d462568209289096e5faef8b042983445
Jente Adams <jente-29@hotmail.com>
2016-02-03 22:30:50 +0100

    added multiplications

--- MathGame.py
+++ MathGame.py
@@ -2,6 +2,7 @@ import pygame, sys, rectangles
 from rectangles import *
 from pygame.locals import *
 
+
 FPS = 50
 WINDOWWIDTH = 640
 WINDOWHEIGHT = 480
@@ -9,9 +10,10 @@ clock = pygame.time.Clock()
 
 def main():
     pygame.init()
+    font = pygame.font.Font("./fonts/arial.ttf", 20)
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     pygame.display.set_caption('Math Game')
-    RECTANGULAR = Rectangle(10, 100)
+    RECTANGULAR = Rectangle(10, font)
     while True:
         clock.tick(FPS)
         DISPLAYSURF.fill( (0,0,0) )
@@ -20,7 +22,7 @@ def main():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-        RECTANGULAR.render(DISPLAYSURF)
+        RECTANGULAR.Render(DISPLAYSURF)
         pygame.display.update()
         
 if __name__ == '__main__':
--- rectangles.py
+++ rectangles.py
@@ -1,18 +1,19 @@
 import pygame, sys
 from pygame.locals import *
+from random import randint
 
 class Rectangle:
     global RED
-    RECTSIZE = 50
-    RECTX = 10
-    RECTY = 100
     RED = (255,0,0)
     
-    def __init__(self, x, y):
-        self.rplayer = pygame.Rect(x, y, 50, 50)
+    def __init__(self, x, font):
+        self.rplayer = pygame.Rect(x, -50, 50, 50)
+        self.multip = Multiplication()
+        self.multipltext = font.render(str(self.multip.val1)+"X"+str(self.multip.val2), True, (0,0,0))
 
-    def render(self, SURF):
+    def Render(self, SURF):
         pygame.draw.rect(SURF, RED, self.rplayer, 0)
+        SURF.blit(self.multipltext, (self.rplayer.x, self.rplayer.y))
         
     def getPos(self):
         return self.rplayer.x, self.rplayer.y
@@ -22,3 +23,8 @@ class Rectangle:
             self.rplayer.y = -50
         else:
             self.rplayer.y +=1
+            
+class Multiplication:
+    def __init__(self):
+        self.val1 = randint(0,10)
+        self.val2 = randint(0,10)
